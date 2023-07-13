d40e82a1ffaee992877096d32f16300ccc2a199f
dikshyant <dikshyant.kashajoo@gmail.com>
2023-07-13 21:02:02 +0545

    loginPage

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [visible, setVisible] = useState(false)
+  return (
+    <div className="min-h-screen flex bg-blue-400 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-4 text-center text-3xl font-extrabold text-gray-500">Login your account</h2>
+      </div>
+      <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+          <form className='space-y-6'>
+            <div>
+              <label htmlFor='email' className='block text-sm font-medium text-gray-500'>Email</label>
+              <div className='mt-1'>
+                <input className='appearance-none block w-full px-3 py-2 border border-gray-500 rounded shadow-sm placeholder-slate-200 focus:outline-none focus:border-blue-300' type='email' name='email' autoComplete='email' required value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
+              </div>
+              <label htmlFor='password' className='block text-sm font-medium text-gray-500'>Password</label>
+              <div className='mt-1 relative'>
+                <input className='appearance-none block w-full px-3 py-2 border border-gray-500 rounded shadow-sm placeholder-slate-200 focus:outline-none focus:border-blue-300' type='password' name='password' autoComplete='current-password' required value={password} onChange={(e) => { setPassword(e.target.value) }}></input>
+               { visible ? <AiOutlineEye
+                  className="absolute right-2 top-2 cursor-pointer"
+                  size={25}
+                  onClick={() => setVisible(false)}
+                />:
+                <AiOutlineEyeInvisible
+                  className="absolute right-2 top-2 cursor-pointer"
+                  size={25}
+                  onClick={() => setVisible(true)}
+                />}
+              </div>
+
+
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
