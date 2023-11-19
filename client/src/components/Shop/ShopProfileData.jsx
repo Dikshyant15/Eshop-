57c41cb0e360b9199d0bbeeacf8f02a5f7f992cd
dikshyant <dikshyant.kashajoo@gmail.com>
2023-11-19 15:38:16 +0545

    commit

--- client/src/components/Shop/ShopProfileData.jsx
+++ client/src/components/Shop/ShopProfileData.jsx
@@ -3,20 +3,24 @@ import styles from "../../styles/styles";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProductsShop } from '../../redux/actions/product';
+import Card from '../Route/ProductCard/Card'
 
 
-const ShopProfileData = () => {
+const ShopProfileData = ({ isOwner }) => {
   const [active, setActive] = useState(1)
 
-  const {product} = useSelector((state)=> state.product)
-  const {id} = useParams()
+  const { product } = useSelector((state) => state.product)
+  const { events } = useSelector((state) => state.event)
+  const { id } = useParams()
   const dispatch = useDispatch()
 
 
-  useEffect(()=>{
+  useEffect(() => {
     getAllProductsShop(id)
-  },[dispatch])
-  
+  }, [dispatch])
+
+  console.log(product)
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -48,10 +52,22 @@ const ShopProfileData = () => {
           </div>
         </div>
         <div>
-          
+          {isOwner && (
+            <div>
+              <Link to="/dashboard">
+                <div className={`${styles.button} !rounded-[4px] h-[42px] mr-5`}>
+                  <span className="text-[#fff]">Go Dashboard</span>
+                </div>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
 
+      {active === 1 && (<div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[22px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+        {product &&
+          product.map((i, index) => (<Card data={i} key={index} />))}
+      </div>)}
     </div>
   );
 };
